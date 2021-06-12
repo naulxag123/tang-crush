7e94026bbdc570f18484344231c5cdf1cbbd540c
naulxag123 <85783955+naulxag123@users.noreply.github.com>
2021-06-12 15:34:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Ngày anh gặp em thì 
+    anh mới phát hiện ra thiên thần
+    là có thật chứ không phải nó chỉ có trong truyện cổ tích mà bà hay kể,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -11,7 +10,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai Tân à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'thật ra thì em cũng yêu anh từ lâu rồi Anh Tân à :> ',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',

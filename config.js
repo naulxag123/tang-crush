607eac2f56d18bfd42855fc60568ec05502c7f9d
naulxag123 <85783955+naulxag123@users.noreply.github.com>
2024-08-19 17:01:39 +0700

    Tan Test

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introTitle: '🖐🖐🖐Xin Chàoo ! 🖐🖐🖐',
     introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
+    btnIntro: 'Dạ',
+    title: 'Anh Biết Em Có Thích Anh Phải Không?? ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em  công nhận emm thích Anh đó nha :> ',
+    btnYes: ' Dạ Thật ra Em Cũng Thích Anh Từ Lâu... 💖💖💖',
+    btnNo: ' Em Chỉ Xem Anh Là Bạn à ! 💔 ',
     question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    reply: ' Tại Vì Anh Dễ Thương Tốt Tính, Xứng Đáng Mười Điểm 😊😊',
     mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
     messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://www.facebook.com/messages/t/100046803243059'
+    btnAccept: 'Dạ 💖',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/7841218372564154'
